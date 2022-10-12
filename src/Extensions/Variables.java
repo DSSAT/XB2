f3c35d4429cb052275115ab15f9c48f643c9f2b3
jazz.jaikla <jazz.jaikla@pcmicorp.com>
2022-10-12 12:55:06 +0700

    Change format date time depend on region format on system

--- src/Extensions/Variables.java
+++ src/Extensions/Variables.java
@@ -1,5 +1,6 @@
 package Extensions;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -11,25 +12,55 @@ public class Variables {
     
     private static SimpleDateFormat dateFormatWithTime;
     private static SimpleDateFormat dateFormat;
-    private static final String DATE_FORMAT = "MM/dd/yyyy";
+    private static String DATE_FORMAT;
+    private static Locale locale;
     
     public static SimpleDateFormat getDateFormatWithTime()
     {
         if(dateFormatWithTime == null)
-            dateFormatWithTime = new SimpleDateFormat(DATE_FORMAT + "hh:mm:ss", new Locale("en","US"));
+            dateFormatWithTime = new SimpleDateFormat(getDatePattern() + "hh:mm:ss", new Locale("en","US"));
         
         return dateFormatWithTime;
+    }
+    
+    private static String getDatePattern(){
+        if (DATE_FORMAT == null || DATE_FORMAT.isEmpty()) {
+            DateFormat formatter = SimpleDateFormat.getDateInstance(SimpleDateFormat.SHORT, getLocale());            
+            
+            String pattern = ((SimpleDateFormat) formatter).toPattern();
+            String localPattern = ((SimpleDateFormat) formatter).toLocalizedPattern();
+            if(!localPattern.contains("MM"))
+                localPattern = localPattern.replaceAll("M", "MM");
+            if(!localPattern.contains("dd"))
+                localPattern = localPattern.replaceAll("d", "dd");
+            if(!localPattern.contains("yyyy"))
+                localPattern = localPattern.replaceAll("yy", "yyyy");
+            
+            DATE_FORMAT = localPattern;
+        }
+        
+        return DATE_FORMAT;
     }
     
     public static SimpleDateFormat getDateFormat()
     {
         if(dateFormat == null)
-            dateFormat = new SimpleDateFormat(DATE_FORMAT, new Locale("en","US"));
+            dateFormat = new SimpleDateFormat(getDatePattern(), new Locale("en","US"));
         
         return dateFormat;
     }
     
+    private static Locale getLocale() {
+        if (locale == null) {
+            locale = System.getProperty("user.language.format") == null
+                    ? Locale.getDefault()
+                    : new Locale(System.getProperty("user.language.format"), System.getProperty("user.country.format"));
+        }
+
+        return locale;
+    }
+    
     public static String getDateFormatString(){
-        return DATE_FORMAT;
+        return getDatePattern();
     }
 }
