d65d1612d0571085d7e8615af0032a34daa47cf2
jazzjaikla <jazzge@hotmail.com>
2024-01-29 09:26:37 +0700

    Change button for add and delete in fertilizer to Add & Delete application

--- src/xbuild/FertilizerFrame.java
+++ src/xbuild/FertilizerFrame.java
@@ -135,7 +135,7 @@ public class FertilizerFrame extends IXInternalFrame {
         rdReportedDates.setText("On Reported Dates");
 
         bnAddLayer.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/Plus.png"))); // NOI18N
-        bnAddLayer.setText("Add Layer");
+        bnAddLayer.setText("Add Application");
         bnAddLayer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 bnAddLayerActionPerformed(evt);
@@ -143,7 +143,7 @@ public class FertilizerFrame extends IXInternalFrame {
         });
 
         bnDeleteLayer.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/Minus.png"))); // NOI18N
-        bnDeleteLayer.setText("Delete Layer");
+        bnDeleteLayer.setText("Delete Application");
         bnDeleteLayer.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 bnDeleteLayerActionPerformed(evt);
